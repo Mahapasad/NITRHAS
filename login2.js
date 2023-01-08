b6b4a14434a5274345c0863c8b1fc026c98c4a4f
Mahapasad <115061315+Mahapasad@users.noreply.github.com>
2023-01-08 07:26:46 +0530

    added initial files.

---
+++ login2.js
@@ -0,0 +1,23 @@
+function validate()
+{
+   var username=document.getElementById("username").value;
+   var password=document.getElementById("password").value
+
+
+
+    if (username=="admin@gmail.com" && password=="asdf") {
+
+    
+    
+        alert("Nacho BC");
+       
+        
+    }
+else{
+
+    alert("login failed");
+    return;
+}
+
+
+}
